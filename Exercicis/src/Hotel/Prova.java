6f20b18cc4c35e0dc0fb41b27ac11ff9770e53d8
mvaquer <mvaquer@institutjaumehuguet.cat>
2026-03-05 19:10:09 +0100

    Actualizació del Main (Info del Hotel)

--- Exercicis/src/Hotel/Prova.java
+++ Exercicis/src/Hotel/Prova.java
@@ -1,7 +1,10 @@
 package Hotel;
 
+import java.util.*;
+
 public class Prova {
         public static void main(String[] args) {
+
 
             Client c1 = new Client("Joan Garcia", "12345678X", 229293848);
             Client c2 = new Client("Marta Pou", "87654321Y", 998877665);
@@ -22,12 +25,15 @@ public class Prova {
             Reserva r2 = new Reserva(2, c2, h303, 2); // Marta a la 303 per 2 dies
 
 
+            System.out.println(" INFORMACIÓ DE L'HOTEL ");
+            System.out.println(elMeuHotel.toString());
+
             System.out.println("\n DETALL DE LES RESERVES ");
             System.out.println(r1.toString());
             System.out.println(r2.toString());
 
-
-            System.out.println("\n LLISTAT D'HABITACIONS DISPONIBLES");
+            // 6. EXEMPLE D'ÚS DE L'ARRAYLIST DE L'HOTEL
+            System.out.println("\n LLISTAT D'HABITACIONS DISPONIBLES ");
             for (Habitacio h : elMeuHotel.getHabitacions()) {
                 System.out.println("Habitació " + h.getId() + " - Preu nit: " + h.getPreuNit() + "€");
             }
